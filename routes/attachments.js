a877caaa6306c5c55021b5bb1dbb8f8704037b2b
Dani J <dani.user@gmail.com>
2018-11-12 21:00:38 +0200

    Implemented uploads

--- routes/attachments.js
+++ routes/attachments.js
@@ -2,31 +2,54 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const mime = require('mime-types');
+const Busboy = require('busboy');
+
 const attachmentStore = require('../services/attachmentStore');
 
 const trustForwardedIP = process.env.TRUST_FORWARDED_IP === 'true';
 const forumBackendUri = process.env.FORUM_BACKEND_URI;
 const forumBackendOrigin = process.env.FORUM_BACKEND_ORIGIN;
 
-function getImpersonationHeaders(req) {
+function getImpersonationIp(req) {
 
-    const ip = trustForwardedIP
+    return trustForwardedIP
         ? req.headers['x-forwarded-for']
         : req.connection.remoteAddress;
+}
 
-    const doubleSubmit = 'empty';
+function getImpersonationCookie(req, doubleSubmit) {
+
     let cookie = "double_submit=" + doubleSubmit;
-
     const auth = req.cookies['auth'];
     if (auth) {
 
         cookie += "; auth=" + auth;
     }
+    return cookie;
+}
+
+function getImpersonationHeaders(req) {
+
+    const doubleSubmit = 'empty';
+
     return {
-        'x-forwarded-for': ip,
+        'x-forwarded-for': getImpersonationIp(req),
         'x-double-submit': doubleSubmit,
-        Cookie: cookie,
+        Cookie: getImpersonationCookie(req, doubleSubmit),
         Origin: forumBackendOrigin
+    }
+}
+
+function getImpersonationHeadersFull(req) {
+
+    const doubleSubmit = req.cookies['double_submit'];
+
+    return {
+        'x-forwarded-for': getImpersonationIp(req),
+        'x-double-submit': req.headers['x-double-submit'] || '',
+        Cookie: getImpersonationCookie(req, doubleSubmit),
+        Origin: req.headers['origin'] || '',
+        Referer: req.headers['referer'] || ''
     }
 }
 
@@ -58,17 +81,16 @@ router.get('/:id', async (req, res) => {
     }
 
     try {
-        const response = await axios({
+        const response = getResponseObject(await axios({
 
             method: 'get',
             url: forumBackendUri + '/attachments/try/' + encodeURIComponent(id),
             headers: getImpersonationHeaders(req)
-        });
+        }));
 
-        const responseObject = getResponseObject(response);
-        const attachment = responseObject ? responseObject.attachment : null;
+        const attachment = response ? response.attachment : null;
 
-        if (( ! attachment) || ( ! attachment.name)) {
+        if (( ! attachment) || (! attachment.name)) {
 
             notFound();
             return;
@@ -97,6 +119,101 @@ router.get('/:id', async (req, res) => {
         res.statusCode = 500;
         res.send('Could not download attachment!');
     }
+});
+
+function acceptAllStatusCodes(status) {
+    return true;
+}
+
+function isStatusOk(status) {
+    return (200 <= status) && (status < 300);
+}
+
+router.post('/', async (req, res) => {
+
+    const canAddResponseData = await axios({
+
+        method: 'post',
+        url: forumBackendUri + '/attachments/can_add',
+        headers: getImpersonationHeadersFull(req),
+        validateStatus: acceptAllStatusCodes
+    });
+    if ( ! isStatusOk(canAddResponseData.status)) {
+
+        res.statusCode = canAddResponseData.status;
+        res.send(canAddResponseData.data);
+        return;
+    }
+    const canAddResponse = getResponseObject(canAddResponseData);
+    const allowedToUpload = canAddResponse.availableBytes || 0;
+
+    const busboy = new Busboy({
+        headers: req.headers,
+        limits: {
+            files: 1,
+            fileSize: allowedToUpload + 1
+        }
+    });
+
+    let upload;
+
+    busboy.on('file', (fieldname, file, filename/*, encoding, mimetype*/) => {
+
+        upload = attachmentStore.prepareAttachmentUpload();
+        upload.name = filename;
+        upload.size = 0;
+
+        file.on('data', function(data) {
+
+            upload.size += data.length;
+        });
+        file.on('end', () => {
+
+            upload.stream.end();
+        });
+        upload.stream.on('finish', async () => {
+
+            if ((1 > upload.size) || (upload.size > allowedToUpload)) {
+
+                await upload.discard();
+                res.writeHead(413); //Payload Too Large
+            }
+            else {
+                try {
+                    const responseData = await axios({
+
+                        method: 'post',
+                        url: forumBackendUri + '/attachments/' + encodeURIComponent(upload.name) + '/' + encodeURIComponent(upload.size),
+                        headers: getImpersonationHeadersFull(req),
+                        validateStatus: acceptAllStatusCodes
+                    });
+                    if ( ! isStatusOk(responseData.status)) {
+                        await upload.discard();
+                    }
+                    else {
+
+                        const response = getResponseObject(responseData);
+                        if (0 == response.status) {
+                            await upload.persist(response.id);
+                        }
+                        else {
+                            await upload.discard();
+                        }
+                    }
+                    res.statusCode = responseData.status;
+                    res.send(responseData.data);
+                }
+                catch(e) {
+
+                    await upload.discard();
+                    res.writeHead(500);
+                }
+            }
+            res.end();
+        });
+        file.pipe(upload.stream);
+    });
+    req.pipe(busboy);
 });
 
 module.exports = router;
--- services/attachmentStore.js
+++ services/attachmentStore.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
+const uuidv4 = require('uuid/v4');
 
 const fileLocation = process.env.FILE_LOCATION;
+const tempFileLocation = process.env.TEMP_FILE_LOCATION;
 
 async function fileExists(filePath) {
 
@@ -9,15 +11,44 @@ async function fileExists(filePath) {
 
         fs.stat(filePath, (err, stats) => {
 
-            if (err) {
-
-                reject(err);
-            }
-            else {
-
-                resolve(stats.size > 0);
-            }
+            if (err) { reject(err); }
+            else { resolve(stats.size > 0); }
         });
+    });
+}
+
+async function deleteFile(filePath) {
+
+    return new Promise((resolve, reject) => {
+
+        fs.unlink(filePath, err => {
+            if (err) { reject(err); }
+            else { resolve(); }
+        })
+    });
+}
+
+async function createDirectory(path) {
+
+    return new Promise((resolve, reject) => {
+
+        fs.mkdir(path, {
+            recursive: true
+        }, err => {
+            if (err && (err.code !== 'EEXIST')) { reject(err); }
+            else { resolve(); }
+        })
+    });
+}
+
+async function renameFile(oldPath, newPath) {
+
+    return new Promise((resolve, reject) => {
+
+        fs.rename(oldPath, newPath, err => {
+            if (err) { reject(err); }
+            else { resolve(); }
+        })
     });
 }
 
@@ -35,5 +66,25 @@ module.exports = {
         if ( ! await fileExists(filePath)) return null;
 
         return fs.createReadStream(filePath);
+    },
+
+    prepareAttachmentUpload: () => {
+
+        const tempId = uuidv4();
+        const tempFilePath = path.join(tempFileLocation, tempId);
+
+        return {
+
+            tempId: tempId,
+            stream: fs.createWriteStream(tempFilePath),
+            totalBytes: 0,
+            discard: () => deleteFile(tempFilePath),
+            persist: async id => {
+
+                const filePath = getAttachmentFilePath(id);
+                await createDirectory(path.dirname(filePath));
+                await renameFile(tempFilePath, filePath);
+            }
+        }
     }
 };
